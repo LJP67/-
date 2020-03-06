311072c79caaa7ac72d42866a64e1f2159cf3d3d
LuJianPing <lujianping67@163.com>
2020-03-07 00:34:42 +0800

    获取收货地址

--- pages/shopping/index.js
+++ pages/shopping/index.js
@@ -1,4 +1,26 @@
 // pages/shopping/index.js
 Page({
+  data:{
+    // 收货地址
+    formdz:{}
+  },
+  // 获取收货地址
+  handshow(){
+    // 看官网有
 
+    wx.chooseAddress({
+      success:(res) =>{
+        this.setData({
+          formdz:{
+              // 姓名
+              name:res.userName,
+              // 电话
+              tel:res.telNumber,
+              // 收货地址
+            detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+      }
+    })
+  },
 })
