d4573d25dd8f76be7a3f92d261408350dd2e3f62
LuJianPing <lujianping67@163.com>
2020-03-11 17:40:56 +0800

    我的页面完成

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -1,5 +1,17 @@
 // pages/mine/index.js
 Page({
 
+  // 收货地址
+  handleAddress() {
+    // 根据接口的名字在文档里搜索
+    wx.chooseAddress();
+  },
 
+  // 联系客服
+  handleContact() {
+    // 根据接口的名字在文档里搜索
+    wx.makePhoneCall({
+      phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+    })
+  }
 })
